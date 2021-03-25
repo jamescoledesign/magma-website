cc98b9358d3d6f1671aa020d64739f7657071880
James Cole <james@openstack.org>
2021-03-25 11:12:25 -0500

    Add section tag

--- src/components/UserStories.js
+++ src/components/UserStories.js
@@ -1,5 +1,8 @@
 import React from "react";
 import Tower from "../../static/img/tower.png";
+import Atalaya from "../../static/img/Atalaya.png";
+import Charity from "../../static/img/Charity.png";
+import Kamol from "../../static/img/Kamol.png";
 
 const UserStories = class extends React.Component {
   constructor(props) {
@@ -11,11 +14,12 @@ const UserStories = class extends React.Component {
     return (
       <React.Fragment>
         <section className="user-stories">
+        <h5 className="section-tag">USER STORIES</h5>
           <div className="three-col">
 
             <div className="user-story-lt-rt">
               <div className="img-container-user-story">
-                <img className="img-lt" src={Tower} />
+                <img className="img-user-story" src={Atalaya} />
                 </div>
                 <h3 className="user-story-title">First user story headline goes in this space</h3>
                 <p>Page layout is where these components come together.</p>
@@ -23,17 +27,17 @@ const UserStories = class extends React.Component {
 
             <div className="user-story-middle">
               <div className="img-container-user-story">
-                <img className="img-lt" src={Tower} />
+                <img className="img-user-story" src={Charity} />
                 </div>
-                <h3 className="user-story-title">First user story headline goes in this space</h3>
+                <h3 className="user-story-title">Second user story headline goes in this space</h3>
                 <p>Page layout is where these components come together.</p>
             </div>
 
-            <div className="user-story">
+            <div className="user-story-lt-rt">
               <div className="img-container-user-story">
-                <img className="img-lt" src={Tower} />
+                <img className="img-user-story" src={Kamol} />
                 </div>
-                <h3 className="user-story-title">First user story headline goes in this space</h3>
+                <h3 className="user-story-title">Third user story headline goes in this space</h3>
                 <p>Page layout is where these components come together.</p>
             </div>
 
