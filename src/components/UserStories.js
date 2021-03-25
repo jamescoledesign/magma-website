0c0fa13acd832190cc6b29521953573a6271a572
James Cole <james@openstack.org>
2021-03-25 11:29:59 -0500

    Add links

--- src/components/UserStories.js
+++ src/components/UserStories.js
@@ -19,16 +19,16 @@ const UserStories = class extends React.Component {
 
             <div className="user-story-lt-rt">
               <div className="img-container-user-story">
-                <img className="img-user-story" src={Atalaya} />
+              <a href="https://www.youtube.com/watch?v=hNHAz6Km-j0&list=PLKqaoAnDyfgolzgeREuKrvsPJhQLTt_he&index=5&t=668s"><img className="img-user-story" src={Atalaya} /></a>
                 </div>
                 <h3 className="user-story-title">Connecting Tribal nations to life-saving resources</h3>
                 <p>MuralNet uses private LTE networks powered by Magma to enable tribal nations to control their internet access.</p>
-                <a href="https://github.com/magma/magma" className="text-cta">Learn More</a>
+                <a href="https://www.youtube.com/watch?v=hNHAz6Km-j0&list=PLKqaoAnDyfgolzgeREuKrvsPJhQLTt_he&index=5&t=668s" className="text-cta">Learn More</a>
             </div>
 
             <div className="user-story-middle">
               <div className="img-container-user-story">
-                <img className="img-user-story" src={Charity} />
+              <a href="https://github.com/magma/magma"><img className="img-user-story" src={Charity} /></a>
                 </div>
                 <h3 className="user-story-title">Deploying Magma to lower network costs</h3>
                 <p>BRCK deploys low cost connectivity in both urban and rural markets in Kenya.</p>
@@ -37,11 +37,11 @@ const UserStories = class extends React.Component {
 
             <div className="user-story-lt-rt">
               <div className="img-container-user-story">
-                <img className="img-user-story" src={Kamol} />
+              <a href="https://www.youtube.com/watch?v=VybSCeTPf88&list=PLKqaoAnDyfgolzgeREuKrvsPJhQLTt_he&index=13&t=917s"><img className="img-user-story" src={Kamol} /></a>
                 </div>
                 <h3 className="user-story-title">Increasing Network Redundancy with Magma Access Gateways</h3>
                 <p>WiConnect Wireless delivers fiber, cable, and wireless-based internet in the southwest region of Wisconsin.</p>
-                <a href="https://github.com/magma/magma" className="text-cta">Learn More</a>
+                <a href="https://www.youtube.com/watch?v=VybSCeTPf88&list=PLKqaoAnDyfgolzgeREuKrvsPJhQLTt_he&index=13&t=917s" className="text-cta">Learn More</a>
             </div>
 
           </div>
