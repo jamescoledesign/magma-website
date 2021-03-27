47eb061ca3e957784a6c3b2567b5ec3e8ac3352c
James Cole <james@openstack.org>
2021-03-27 13:14:58 -0500

    Add logo link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,41 +12,46 @@ const Navbar = class extends React.Component {
   render() {
     return(
       <nav className="static-nav">
-      <div className="navbar-primary">
-        <div className="nav-logo"><img src={NavLogo} alt="Magma Logo" /></div>
-     
-          <div className="subnav">
-            <button className="subnavbtn">About</button>
-          </div> 
+        <div className="navbar-primary">
+        
+        <div className="nav-logo">
+          <Link to="/" title="Magma Website Home">
+            <img src={NavLogo} alt="Magma Logo" />
+          </Link>
+        </div>
+      
+        <div className="subnav">
+          <button className="subnavbtn">About</button>
+        </div> 
 
-          <div className="subnav">
-            <button className="subnavbtn">User Stories</button>
-          </div> 
+        <div className="subnav">
+          <button className="subnavbtn">User Stories</button>
+        </div> 
 
-          <div className="subnav">
-            <button className="subnavbtn">Community<i className="fa fa-caret-down"></i></button>
-            <div className="subnav-content">
-              <div className="arrow-up"></div>
-              <div className="subnav-item"><a href="#company">Governance</a></div>
+        <div className="subnav">
+          <button className="subnavbtn">Community<i className="fa fa-caret-down"></i></button>
+          <div className="subnav-content">
+            <div className="arrow-up"></div>
+            <div className="subnav-item"><a href="#company">Governance</a></div>
               <div className="subnav-item"><a href="#team">Magma Dev Conf 2021</a></div>
             </div>
-          </div> 
+        </div> 
 
-          <div className="subnav">
-            <button className="subnavbtn">Docs</button>
-          </div> 
-          
-          <div className="subnav">
-            <button className="subnavbtn">Supporters</button>
-          </div> 
+        <div className="subnav">
+          <button className="subnavbtn">Docs</button>
+        </div> 
+            
+        <div className="subnav">
+          <button className="subnavbtn">Supporters</button>
+        </div> 
 
-          <div class="subnav">
-            <button class="subnavbtn">News<i class="fa fa-caret-down"></i></button>
-            <div class="subnav-content-news">
+        <div class="subnav">
+          <button class="subnavbtn">News<i class="fa fa-caret-down"></i></button>
+          <div class="subnav-content-news">
             <div className="arrow-up-news"></div>
             <div className="subnav-item"><a href="#team">Blog</a></div>
-            </div>
-          </div> 
+          </div>
+        </div> 
 
       </div>
     </nav>
