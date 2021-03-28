839491ee694fb476db3a3e354958a833baa42fc8
James Cole <james@openstack.org>
2021-03-28 12:55:09 -0500

    Edit nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -61,14 +61,14 @@ const Navbar = class extends React.Component {
               id="navMenu"
               className={`nav-content ${this.state.navBarActiveClass}`}
             >
-      
+      {/*
         <div className="subnav">
           <Link to="/#about"><button className="subnavbtn">About</button></Link>
         </div> 
 
         <div className="subnav">
         <Link to="/#user-stories"><button className="subnavbtn">User Stories</button></Link>
-        </div> 
+        </div> */}
 
         <div className="subnav">
         <a href="/community"><button className="subnavbtn">Community<i className="fa fa-caret-down"></i></button></a>
@@ -82,17 +82,19 @@ const Navbar = class extends React.Component {
         <div className="subnav">
         <OutboundLink href="https://docs.magmacore.org/docs/basics/introduction.html" rel="noopener noreferrer"><button className="subnavbtn">Docs</button></OutboundLink>
         </div> 
-            
+
+        {/*    
         <div className="subnav">
         <Link to="/#supporters"><button className="subnavbtn">Supporters</button></Link>
         </div> 
+        */}
 
         <div class="subnav">
-        <Link to="/blog"><button class="subnavbtn">News<i class="fa fa-caret-down"></i></button></Link>
-          <div class="subnav-content-news">
+        <Link to="/blog"><button class="subnavbtn">Blog<i class="fa fa-caret-down"></i></button></Link>
+          {/*<div class="subnav-content-news">
             <div className="arrow-up-news"></div>
             <div className="subnav-item"><a href="/blog">Blog</a></div>
-          </div>
+          </div>*/}
         </div> 
 
       </div></div>
